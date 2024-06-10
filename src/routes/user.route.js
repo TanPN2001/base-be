2090228ba847a991b3c3c8cd6b6eac11ba146656
TanPN2001 <ronaltanface@gmail.com>
2024-06-10 14:59:18 +0700

    husky

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const userControllers = require("../controllers/user.controller.js");
-console.log(t);
+// console.log(t);
 
 router.get("/user/test", userControllers.test);
 module.exports = router;
